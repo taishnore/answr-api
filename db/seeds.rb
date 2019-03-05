0eac48864b7951ae28c48fbc0bcc34b57407d289
github username <github email address>
2019-03-05 17:27:32 -0500

    edited seeds, fixed game_serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-User.destroy_all
+
 Meme.destroy_all
 Game.destroy_all
 Round.destroy_all
@@ -14,21 +14,9 @@ Round.destroy_all
 
  ]
 
-User.create(name: "Taimur", email: "taimurmshah@gmail.com")
 
-Meme.create(
-  title: "Elon",
-  url: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2018/09/07/105438875-1536336503523screen-shot-2018-09-07-at-12.07.32-pm.1910x1000.jpg",
-  user_id: User.find_by(name: "Taimur")
-)
 
-Meme.create(
-  title: "Neil",
-  url: "https://ametia.files.wordpress.com/2014/03/neil-degrasse-tyson.jpg",
-  user_id: User.find_by(name: "Taimur")
-)
-
-Game.create(title: "The Game of Life and Death", number_of_rounds: 3)
+Game.create(title: "The Game of Life and Death", number_of_rounds: 3, is_game_in_play: true)
 Round.create(game_id: Game.find_by(title: "The Game of Life and Death").id, prompt: @prompts.sample)
 Round.create(game_id: Game.find_by(title: "The Game of Life and Death").id, prompt: @prompts.sample)
 Round.create(game_id: Game.find_by(title: "The Game of Life and Death").id, prompt: @prompts.sample)
